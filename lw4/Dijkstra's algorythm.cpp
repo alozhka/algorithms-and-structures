95da6bc37e8395633ff29a68e11f5c05f68eeb45
Sergey <100339481+alozhka@users.noreply.github.com>
2022-09-29 00:22:22 +0300

    some pieces of implementation

---
+++ lw4/Dijkstra's algorythm.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+#include "../lw3/Stack.h"
+
+int main()
+{
+    Stack *stack = new Stack;
+    stack == NULL;
+    string s;
+
+    while (0 == 0)
+    {
+        if (!EOF) cin >> s;
+        else break;
+        
+        switch(s)
+        {
+            case "sin:"
+            {
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+}
